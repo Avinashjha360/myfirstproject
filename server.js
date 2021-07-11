5e197f41a88bdab98f026499ef6d0c34018622c0
Avinash jha <80147639+Avinashjha360@users.noreply.github.com>
2021-07-12 01:45:57 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const fs=require('fs');
+const express=require('express');
+const app=express();
+const home=fs.readFileSync('./zpay.html');
+const payment=fs.readFileSync('./payment.html');
+const contact=fs.readFileSync('./contact.html');
+
+const port=900;
+
+app.use(express.urlencoded());
+app.use('/static',express.static('static'));
+
+app.get("/",(req,res) =>{
+    res.setHeader('Content-Type','text/html');
+    res.status(200).send(home);
+})
+app.get("/zpay.html",(req,res) =>{
+    res.setHeader('Content-Type','text/html');
+    res.status(200).send(home);
+})
+app.get("/payment.html",(req,res) =>{
+    res.setHeader('Content-Type','text/html');
+    res.status(200).send(payment);
+})
+app.get("/contact.html",(req,res) =>{
+    res.setHeader('Content-Type','text/html');
+    res.status(200).send(contact);
+})
+
+app.listen(port,() => {
+    console.log(`Server running at ${port}`);
+})
